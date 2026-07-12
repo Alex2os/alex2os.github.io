1187ab8b6c3af86f3aee92b9813137927a79d359
zJaviOne <imalex1ne@gmail.com>
2026-07-12 09:00:14 -0600

    solve button color by section in the nav bar

--- navBarButtons.js
+++ navBarButtons.js
@@ -3,12 +3,13 @@
 const NavBarButtons = document.querySelectorAll(".default_nav_button");
 
 // options for the observer
-// seems that for now using a threshold of 0 (zero) and using the horizontal line on the screen (root margin) it works correctly. still this may cause visual-
-// bugs, but for now this solution works for all sections.
+// using a rootmargin of 0px and a threshold of 0.5 is enough for what we want in the navigation bar. previous visual bugs were solved by-
+// instead of grabbing the whole sections (see commented code at the end of this file) we now grab the id's by themself, and it seems this is the correct way-
+// to do this.
 const Options = {
     root: null,
-    rootMargin: "-50% 0px -49% 0px",
-    threshold: 0,
+    rootMargin: "0px",
+    threshold: 0.5,
 };
 
 // function callback for the observer. here we do all the logic needed to check the intersections with the sections and changing the color of the buttons
@@ -33,10 +34,23 @@ const SectionsObserverCallback = (entries, observer) => {
 
 // we declare our observer to check for all the sections in the page
 const Observer = new IntersectionObserver(SectionsObserverCallback, Options);
+
 // we can obtain all the sections in our document with the following line of code. reminder that we obtain the <section> elements in the html.
-const Sections = document.querySelectorAll("section");
+// const Sections = document.querySelectorAll("section");
 
 // then for all the sections that we obtained we add each one to the observer.
+/*
 Sections.forEach(section => {
     Observer.observe(section);
 });
+*/
+
+// we obtain the sections in our code to add an observer to them. we don't choose the sections  as in the commented code, as that ruins the purpose of the-
+// options threshold of 0.5, as if we grab the sections by themself, for bigger sections (like the games section) it will not work properly. so we-
+// grab the sections by id ("#games", "#projects", etc.) and then add an observer to them.
+Observer.observe(document.querySelector("#home"));
+Observer.observe(document.querySelector("#about_me"));
+Observer.observe(document.querySelector("#projects"));
+Observer.observe(document.querySelector("#games"));
+Observer.observe(document.querySelector("#contact"));
+
